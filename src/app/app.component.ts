58f90a181a991448dca266214128770221ba0799
Sonam Shrestha <sonam.shrestha@sbsolutionsnepal.com>
2022-08-12 16:36:10 +0545

    S

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-manageent-front';
+  data
+    = [];
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep',
+    'Walk Dog',
+    'Stretch',
+    'Code Stuff',
+    'Drag Stuff',
+    'Drop Stuff',
+    'Run',
+    'Walk'
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+  ngOnInit(): void {
+    for (let i = 0; i < 6; i++) {
+  this.data.push({
+        balance: '$3,946.45',
+          picture: 'http://placehold.it/32x32',
+          age: 23,
+          name: 'Bird Ramsey',
+          gender: 'male',
+          company: 'NIMON ' + i,
+          email: 'birdramsey@nimon.com'
+      });
+    }
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
